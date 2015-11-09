95458736854bf1029faf56565a82f8af9024c7c2
MacMalainey <mac@malainey.com>
2015-11-09 09:32:05 -0700

    Attached servo

---
+++ Car/Car.ino
@@ -0,0 +1,48 @@
+#include <Servo.h>
+
+#include <Stepper.h>
+
+#include <Wire.h>
+const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+int servoPin = 13;
+Servo myServo;
+
+// for your motor
+void setup() {
+  // put your setup code here, to run once:
+    Wire.begin(8);
+    myServo.attach(servoPin);
+    Serial.begin(9600);
+    Wire.onReceive(Controller);
+}
+int A = 0;
+int B = 0;
+int C = 0;
+int D = 0;
+int TURN = 0;
+bool GAS = false;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+	myStepper.step(stepsPerRevolution/100);
+}
+void Controller(int numBytes){
+	Serial.println("RECEIVED");
+	while(Wire.available()){
+		int test = Wire.read();
+		if (test == 72){
+			myStepper.setSpeed(10);
+		}
+		else if (test == 76){
+			myStepper.setSpeed(0);
+		}
+		else{
+			TURN = test;
+			Serial.println(TURN);
+			myServo.write(TURN);
+		}
+	}
+
+
+}
--- Controller/controller.ino
+++ Controller/controller.ino
@@ -1,9 +1,13 @@
+#include <Wire.h>
+
+
 #define button 3
 #define pot A2
 // define button and potentiometer pins
 void setup(){
   pinMode(button, INPUT_PULLUP);
   Serial.begin(9600);
+  Wire.begin();
 }
 int lastPotState = 0;
 int lastButtonState = 0;
@@ -12,15 +16,28 @@ void loop(){
   if (buttonState == LOW and lastButtonState != buttonState){
     Serial.println("PRESSED");
     lastButtonState = buttonState;
+    Wire.beginTransmission(8);
+    Wire.write("H");
+    Wire.endTransmission();
   }
   else if (lastButtonState != buttonState){
     Serial.println("NOT");
+    Wire.beginTransmission(8);
+    Wire.write("L");
+    Wire.endTransmission();
     lastButtonState = buttonState;
   }
   
   int potState = analogRead(pot);
+  potState = potState / 8;
   if (potState != lastPotState and potState != (lastPotState + 1) and potState != (lastPotState - 1)) {
+    if (potState == 72 or potState == 76){
+        potState++;
+    }
     Serial.println(potState);
     lastPotState = potState;
+    Wire.beginTransmission(8);
+    Wire.write(potState);
+    Wire.endTransmission();
   }
 }
